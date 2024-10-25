f6f0b10d9f76f0d74171ca1faf05b13cdba97f7f
mechanicalcodingthunder <132193226+mechanicalcodingthunder@users.noreply.github.com>
2024-10-25 21:25:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function getDirectory(dirname,roll) {
-  let response = await fetch('https://api.github.com/mechanicalcodingthunder/oiexam/tree/main/Files/')
+  let response = await fetch('https://github.com/mechanicalcodingthunder/oiexam/tree/main/Files/')
   // let response = await fetch(dirname);
   console.log(response)
   let str = await response.text();
