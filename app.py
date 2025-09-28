8d54ffc9f10341377ea087b122a85e153984e156
mechanicalcoders <121671478+mechanicalcoders@users.noreply.github.com>
2025-09-28 22:22:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,35 +5,40 @@ from PIL import Image
 import pdfplumber
 from pytesseract import image_to_string
 from pdf2image import convert_from_path
+
 app = Flask(__name__)
-app.secret_key = 'fhewhfiowe hsdvnio'
+app.secret_key = "fhewhfiowe hsdvnio"
 import os
+
 print("hel")
 This_folder = os.listdir("./Files/")
 print(This_folder)
 # filename = os.path.join(THIS_FOLDER, path_to_pdf)
-file= This_folder[3]
-path_to_pdf = "./Files/"+file;
+file = This_folder[3]
+path_to_pdf = "./Files/" + file
 pdf = pdfplumber.open(path_to_pdf)
 first_page = pdf.pages[0]
-tables = first_page.extract_tables()
-rows = [];
+tables = first_page.extract_tables(line_dir="btt")
+rows = []
 for table in tables:
     for row in table:
         rows.append(row)
         print(row)
         print(len(row))
 
-@app.route('/')
-def home():
-  table = rows # indent for pretty-printing
-  print(table)
-  return render_template('index.html')
 
-@app.route("/data_recieve",methods=['GET','POST'])
+@app.route("/")
+def home():
+    table = rows  # indent for pretty-printing
+    print(table)
+    return render_template("index.html")
+
+
+@app.route("/data_recieve", methods=["GET", "POST"])
 def data_recieve():
-    data = rows;
+    data = rows
     return jsonify(data)
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+# if __name__=="__main__":
+#     app.run(debug=True)
