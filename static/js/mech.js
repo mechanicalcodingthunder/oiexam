d181c511f5dbd7e26bb5ff61c5513b863f7c1f35
mechanicalcodingthunder <132193226+mechanicalcodingthunder@users.noreply.github.com>
2025-10-01 15:47:21 +0530

    c

--- static/js/mech.js
+++ static/js/mech.js
@@ -1,6 +1,35 @@
+window.onload = function () {
+  console.log('Loaded')
+}
+
+
+
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
+
+const branch = {
+  "Civil": 0, "Computer": 1, "Electrical": 2, "ECE": 3, "FAA": 4, "FD": 5,
+  "FT": 6, "IC": 7, "Mechanical": 8, "Med Eltx": 9, "TD": 10, "TP": 11, "TT": 12,
+}
+function read_data(data, trgt) {
+  init();
+  console.log(branch[trgt])
+  document.getElementById("output").innerText = data[branch[trgt]];
+}
+function init() {
+  document.getElementById("output").innerText = "";
+}
 function read_file(input) {
-  const input_file = input;
-  console.log(input_file);
+  const dropdown = document.querySelector(".menu");
+  let trgt;
+  dropdown.addEventListener("click", function (e) {
+    if (e.target.tagName === "A") {
+      e.preventDefault();
+      trgt = e.target.textContent;
+    }
+  })
+  const input_file =input;
   Papa.parse(input_file, {
     header: false, // Since your file has irregular/multi-line headers
     download: true,
@@ -8,19 +37,11 @@ function read_file(input) {
     // newline: "\n", // Let PapaParse auto-detect newlines
     complete: function (results) {
       console.log(results.data);
-      document.getElementById("output").innerText = results.data[20];
+      read_data(results.data, trgt)
     },
     error: function (error) {
       console.error("Error parsing CSV:", error);
     },
-  });
-}
-
-document.querySelector("#close").addEventListener("click", function () {
-  document.querySelector(".popup").style.display = "none";
-});
-
-let obj;
-function read_data(data) {
-  obj = data;
+  })
+  document.querySelector(".popup").style.display = "block";
 }
