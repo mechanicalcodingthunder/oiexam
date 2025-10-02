2b611a6b7bcc52213490f2fd3fcef0e59397a332
mechanicalcoders <121671478+mechanicalcoders@users.noreply.github.com>
2025-10-02 20:08:00 +0530

    c

--- app.py
+++ app.py
@@ -2,20 +2,11 @@ from flask import Flask, render_template, jsonify, request
 import pandas as pd
 import numpy as np
 import pdfplumber
+import json
 
 app = Flask(__name__)
 app.secret_key = "fhewhfiowe hsdvnio"
 import os
-
-print("hel")
-This_folder = os.listdir("./Files/")
-print(This_folder)
-# filename = os.path.join(THIS_FOLDER, path_to_pdf)
-file = This_folder[5]
-path_to_pdf = "./Files/" + file
-pdf = pdfplumber.open(path_to_pdf)
-all_tables = []
-print(path_to_pdf)
 
 
 def clean_table(table):
@@ -31,19 +22,88 @@ def clean_table(table):
     return cleaned_table
 
 
-for page in pdf.pages:
-    tb = page.extract_table({"text_line_dir": "btt"})
-    print(tb[0])
-    cleaned_table = clean_table(tb)
-    print(cleaned_table)
-    if page == pdf.pages[0]:
-        df = pd.DataFrame(cleaned_table)
-    else:
-        df = pd.DataFrame(cleaned_table[8:])
-    all_tables.append(df)
-combined_df = pd.concat(all_tables, ignore_index=True)
-print(combined_df.head())
-combined_df.to_csv("output2.csv", index=False, header=False)
+def read_pdf(path_to_pdf):
+    pdf = pdfplumber.open(path_to_pdf)
+    all_tables = []
+    print(path_to_pdf)
+    for page in pdf.pages:
+        tb = page.extract_table({"text_line_dir": "btt"})
+        print(tb[0])
+        cleaned_table = clean_table(tb)
+        print(cleaned_table)
+        if page == pdf.pages[0]:
+            df = pd.DataFrame(cleaned_table[:-1])
+        else:
+            df = pd.DataFrame(cleaned_table[8:-1])
+        all_tables.append(df)
+    return all_tables
+
+
+def write_files(table, filename):
+    combined_df = pd.concat(table, ignore_index=True)
+    print(combined_df.head())
+    combined_df.to_csv(f"{filename}.csv", index=False, header=False)
+
+
+This_folder = os.walk("./")
+print(This_folder)
+Input_path = []
+Output_path = []
+for dirpath, dirname, filenames in This_folder:
+    # print(dirpath, "\n", dirname, "\n", filenames)
+    # print(filenames)
+    if dirpath == "./Files":
+        print(dirpath, filenames)
+        for dir in dirname:
+            Input_path.append(os.path.join(dirpath, dir))
+
+    elif dirpath == "./Results":
+        for dir in dirname:
+            Output_path.append(os.path.join(dirpath, dir))
+
+print(Input_path)
+print(Output_path)
+counter = 0
+for path in Input_path:
+    files = os.listdir(path)
+    counter = counter + 1
+    print(counter)
+    if files:
+        Output_path = []
+        for file in files:
+            if file.endswith(".pdf"):
+                path_to_pdf = os.path.join(path, file)
+                table = read_pdf(path_to_pdf)
+                print((path_to_pdf.split("/")[3]).split("."))
+                Output_filename = (
+                    path_to_pdf.split("/")[0]
+                    + "/Results/"
+                    + path_to_pdf.split("/")[2]
+                    + "/"
+                    + (path_to_pdf.split("/")[3]).split(".")[0]
+                )
+                write_files(table, Output_filename)
+                Output_path.append(f"{Output_filename}.csv")
+        json_file = (
+            path_to_pdf.split("/")[0]
+            + "/Results/"
+            + path_to_pdf.split("/")[2]
+            + "/"
+            + "index.json"
+        )
+        print(json_file)
+        # for input in Input_path:
+        with open(json_file, "w") as f:
+            json.dump({"Files": Output_path}, f, indent=2)
+#     print(os.listdir(input))
+# filename = os.path.join(THIS_FOLDER, path_to_pdf)
+# file = This_folder[5]
+# path_to_pdf = "./Files/" + file
+# pdf = pdfplumber.open(path_to_pdf)
+# all_tables = []
+# print(path_to_pdf)
+
+
 # print(tables)
 # rows = []
 # print("TABLE")
--- static/js/mech.js
+++ static/js/mech.js
@@ -1,30 +1,87 @@
 window.onload = function () {
-  console.log('Loaded')
-}
+  console.log("laoded");
+  fetch_file();
+};
 
-const branch = {
-  "Civil": 0, "Computer": 1, "Electrical": 2, "ECE": 3, "FAA": 4, "FD": 5,
-  "FT": 6, "IC": 7, "Mechanical": 8, "Med Eltx": 9, "TD": 10, "TP": 11, "TT": 12,
+function fetch_file() {
+  const fil = document.querySelector(".selected").innerText;
+  console.log(fil);
+  const url = "./Results/" + fil;
+  fetch(url + "/index.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("File not found");
+      }
+      return res.json();
+    })
+    .then((file) => {
+      console.log(file);
+      // read_file("output2.csv");
+    })
+    .catch((error) => {
+      console.error("Error Fetching", error);
+    });
 }
+const branch = {
+  Civil: 0,
+  Computer: 1,
+  Electrical: 2,
+  ECE: 3,
+  FAA: 4,
+  FD: 5,
+  FT: 6,
+  IC: 7,
+  Mechanical: 8,
+  "Med Eltx": 9,
+  TD: 10,
+  TP: 11,
+  TT: 12,
+};
 function read_data(data, trgt) {
   init();
-  console.log(branch[trgt])
-  let reu = data[branch[trgt]]
-  console.log([reu.slice(0, 3), reu.slice(-3)])
+  let dat = [];
+  data.forEach((element) => {
+    let count = 0;
+    // console.log(element);
+    for (let row of element) {
+      if (row === "220042700002") {
+        dat.push(element);
+        console.log(row);
+        console.log("hell");
+        break;
+      } else if (count === 5) {
+        break;
+      } else {
+        count = count + 1;
+      }
+    }
+  });
+  console.log(dat);
+  let reu = data[branch[trgt]];
+  console.log(reu);
+  // console.log(reu.slice(0, 3).concat(reu.slice(-3)));
   document.getElementById("output").innerText = data[branch[trgt]];
 }
 function init() {
   document.getElementById("output").innerText = "";
 }
+function branch_select(e) {
+  console.log(e);
+  const selected = document.querySelector(".selected");
+  selected.innerText = e.textContent;
+  fetch_file();
+  //   }
+  // });
+}
 function read_file(input) {
   const dropdown = document.querySelector(".menu");
-  let trgt = "Mechanical"
+  let trgt = document.querySelector(".selected").innerText;
   dropdown.addEventListener("click", function (e) {
     if (e.target.tagName === "A") {
       e.preventDefault();
       trgt = e.target.textContent;
     }
-  })
+  });
   const input_file = input;
   Papa.parse(input_file, {
     header: false, // Since your file has irregular/multi-line headers
@@ -33,11 +90,10 @@ function read_file(input) {
     // newline: "\n", // Let PapaParse auto-detect newlines
     complete: function (results) {
       console.log(results.data);
-      read_data(results.data, trgt)
+      read_data(results.data, trgt);
     },
     error: function (error) {
       console.error("Error parsing CSV:", error);
     },
-  })
+  });
 }
-read_file("output2.csv")
