0504c75909454936d3e943cf801788f360e465e3
mechanicalcoders <121671478+mechanicalcoders@users.noreply.github.com>
2025-10-03 21:57:44 +0530

    c

--- static/js/mech.js
+++ static/js/mech.js
@@ -5,6 +5,7 @@ window.onload = function () {
 // Select the branch on click
 let data_file = [];
 
+// function to select the branch
 function branch_select(e) {
   console.log(e);
   e.preventDefault;
@@ -13,18 +14,23 @@ function branch_select(e) {
   fetch_file();
 }
 
+// function to create the table of a student for the input roll no
 function create_table(obj) {
   console.log(obj);
   const tbl =
     document.getElementsByClassName("table_container")[0].lastElementChild;
   const p_ele =
     document.getElementsByClassName("table_container")[0].firstElementChild;
+
+  // check if body is present in table
   if (tbl.childElementCount == 2) {
     if (tbl.classList.contains("hide")) {
       tbl.classList.toggle("hide");
     }
+    // select body of table if present
     tbd = tbl.children[1];
   } else {
+    // create body of table if not there
     tbd = document.createElement("tbody");
     tbl.classList.toggle("hide");
   }
@@ -34,26 +40,32 @@ function create_table(obj) {
     p_ele.innerHTML = "Final Results";
   }
   const tr = document.createElement("tr");
-
-  for (let j of obj[0]) {
+  // loop to create cells in row of table
+  for (let j of obj) {
     console.log(j);
     const td = document.createElement("td");
     td.appendChild(document.createTextNode(j));
     tr.appendChild(td);
   }
-  tbd.insertBefore(tr, tbd.firstChild);
+  // appendrow to tbody
+  tbd.appendChild(tr);
+  // appendbody to table
   tbl.appendChild(tbd);
 }
 
+// function to delete table data if there
 function delete_table() {
-  const tbl = document.getElementById("table");
-  tr1 = tbl.children[0].children[0];
-  tr2 = tbl.children[0].children[1];
-  while (tr1.hasChildNodes()) {
-    tr1.removeChild(tr1.firstChild);
-  }
-  while (tr2.hasChildNodes()) {
-    tr2.removeChild(tr2.firstChild);
+  const tbl =
+    document.getElementsByClassName("table_container")[0].lastElementChild;
+  const p_ele =
+    document.getElementsByClassName("table_container")[0].firstElementChild;
+  if (tbl.childElementCount == 2) {
+    tr = tbl.children[1];
+    console.log(tr);
+    while (tr.hasChildNodes()) {
+      tr.removeChild(tr.firstChild);
+    }
+    p_ele.innerHTML = "No result Found";
   }
   if (tbl.previousElementSibling.classList.contains("hide")) {
     tbl.previousElementSibling.classList.remove("hide");
@@ -65,36 +77,44 @@ function create_data(value) {
   let final_result = [];
   console.log(value);
   console.log(data_file);
-  data_file.forEach((element) => {
-    let f = read_data(element, value);
-    if (f.length > 0) {
-      final_result.push(f); // store if not empty
+  if (data_file.length > 0) {
+    data_file.forEach((element) => {
+      let f = read_data(element, value);
+      if (f.length > 0) {
+        if (final_result.length == 0) {
+          final_result.push(f); // store if not empty
+        } else if (f.length > 1) {
+          // console.log(f);
+          // console.log(f.slice(1));
+          final_result.push(f.slice(1));
+        }
+      }
       // break;                 // stop loop if you want first match
-    }
-  });
-  document.getElementById("output").innerText = final_result;
+    });
+  }
   console.log(final_result);
-  if (final_result.length > 0) {
+  if (final_result.length > 1) {
     for (const result of final_result) {
-      console.log(result.length);
       console.log(result);
-      create_table(result);
+      create_table(result[0]);
     }
   }
 }
 
+// show resutls function
 function showresult(evt) {
+  delete_table();
   const rollno = evt.target.previousElementSibling;
-  console.log(rollno);
   if (rollno.value == "" || rollno.value.length < 12) {
     rollno.focus();
   } else {
     create_data(rollno.value);
   }
 }
+
+// file fetch function
 function fetch_file() {
   const fil = document.querySelector(".selected").innerText;
-  console.log(fil);
   const url = "./Results/" + fil;
   fetch(url + "/index.json")
     .then((res) => {
@@ -112,31 +132,7 @@ function fetch_file() {
     });
 }
 
-function read_data(data, value) {
-  let dat = [];
-  data.forEach((element) => {
-    let count = 0;
-    console.log(element);
-    for (let row of element) {
-      if (row === value) {
-        dat.push(element.slice(0, 3).concat(element.slice(-3)));
-        console.log(dat);
-        // // console.log("hell");
-        // console.log(dat[0].slice(0, 3).concat(dat[0].slice(-3)));
-        break;
-      } else if (count === 5) {
-        break;
-      } else {
-        count = count + 1;
-      }
-    }
-  });
-  return dat;
-}
-function init() {
-  document.getElementById("output").innerText = "";
-}
-
+// file read function
 function read_file(input) {
   for (const input_file of input) {
     Papa.parse(input_file, {
@@ -152,4 +148,37 @@ function read_file(input) {
       },
     });
   }
+}
+
+// function to read data for specific roll no.
+function read_data(data, value) {
+  let dat = [];
+  data.forEach((element) => {
+    let count = 0;
+    console.log(element);
+    for (let row of element) {
+      console.log(row);
+      if (row == "S.No.") {
+        const val = element.slice(0, 3).concat(element.slice(-3));
+        val.unshift("Exam_Year");
+        // console.log(val);
+        dat.push(val);
+        console.log(element.slice(0, 3).concat(element.slice(-3)));
+        break;
+      } else if (row === value) {
+        const val = element.slice(0, 3).concat(element.slice(-3));
+        const year = data[1][0].split(":");
+        val.unshift(year[1]);
+        // console.log(val);
+        dat.push(val);
+        console.log(dat);
+        break;
+      } else if (count === 5) {
+        break;
+      } else {
+        count = count + 1;
+      }
+    }
+  });
+  return dat;
 }
