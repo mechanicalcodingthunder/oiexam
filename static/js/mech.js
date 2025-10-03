08cfea68c62c2979bc205bd3a879828c965c5094
mechanicalcodingthunder <132193226+mechanicalcodingthunder@users.noreply.github.com>
2025-10-03 16:37:28 +0530

    c

--- static/js/mech.js
+++ static/js/mech.js
@@ -3,6 +3,8 @@ window.onload = function () {
   fetch_file();
 };
 // Select the branch on click
+let data_file = [];
+
 function branch_select(e) {
   console.log(e);
   e.preventDefault;
@@ -11,6 +13,79 @@ function branch_select(e) {
   fetch_file();
 }
 
+function create_table() {
+  const tbl =
+    document.getElementsByClassName("table_container")[0].lastElementChild;
+  const p_ele =
+    document.getElementsByClassName("table_container")[0].firstElementChild;
+  col = 5;
+  if (tbl.childElementCount == 2) {
+    if (tbl.classList.contains("hide")) {
+      tbl.classList.toggle("hide");
+    }
+    tbd = tbl.children[1];
+  } else {
+    tbd = document.createElement("tbody");
+    tbl.classList.toggle("hide");
+  }
+  if (tbd.childElementCount == 0) {
+    p_ele.innerHTML = "Final Result";
+  } else {
+    p_ele.innerHTML = "Final Results";
+  }
+  const tr = document.createElement("tr");
+
+  const obj = "hello";
+  for (let j of obj) {
+    const td = document.createElement("td");
+    td.appendChild(document.createTextNode(j));
+    tr.appendChild(td);
+  }
+  tbd.insertBefore(tr, tbd.firstChild);
+  tbl.appendChild(tbd);
+}
+
+function delete_table() {
+  const tbl = document.getElementById("table");
+  tr1 = tbl.children[0].children[0];
+  tr2 = tbl.children[0].children[1];
+  while (tr1.hasChildNodes()) {
+    tr1.removeChild(tr1.firstChild);
+  }
+  while (tr2.hasChildNodes()) {
+    tr2.removeChild(tr2.firstChild);
+  }
+  if (tbl.previousElementSibling.classList.contains("hide")) {
+    tbl.previousElementSibling.classList.remove("hide");
+    tbl.classList.add("hide");
+  }
+}
+
+function create_data(value) {
+  let final_result = [];
+  console.log(value);
+  console.log(data_file);
+  data_file.forEach((element) => {
+    let f = read_data(element, value);
+    if (f.length > 0) {
+      final_result.push(f); // store if not empty
+      // break;                 // stop loop if you want first match
+    }
+  });
+  document.getElementById("output").innerText = final_result;
+  console.log(final_result);
+  create_table();
+}
+
+function showresult(evt) {
+  const rollno = evt.target.previousElementSibling;
+  console.log(rollno);
+  if (rollno.value == "" || rollno.value.length < 12) {
+    rollno.focus();
+  } else {
+    create_data(rollno.value);
+  }
+}
 function fetch_file() {
   const fil = document.querySelector(".selected").innerText;
   console.log(fil);
@@ -30,33 +105,18 @@ function fetch_file() {
       console.error("Error Fetching", error);
     });
 }
-const branch = {
-  Civil: 0,
-  Computer: 1,
-  Electrical: 2,
-  ECE: 3,
-  FAA: 4,
-  FD: 5,
-  FT: 6,
-  IC: 7,
-  Mechanical: 8,
-  "Med Eltx": 9,
-  TD: 10,
-  TP: 11,
-  TT: 12,
-};
-function read_data(data) {
-  init();
+
+function read_data(data, value) {
   let dat = [];
   data.forEach((element) => {
     let count = 0;
-    // console.log(element);
+    console.log(element);
     for (let row of element) {
-      if (row === "220042700059") {
-        dat.push(element);
-        console.log(row);
-        console.log("hell");
-        console.log(dat[0].slice(0, 3).concat(dat[0].slice(-3)));
+      if (row === value) {
+        dat.push(element.slice(0, 3).concat(element.slice(-3)));
+        console.log(dat);
+        // // console.log("hell");
+        // console.log(dat[0].slice(0, 3).concat(dat[0].slice(-3)));
         break;
       } else if (count === 5) {
         break;
@@ -65,8 +125,7 @@ function read_data(data) {
       }
     }
   });
-  console.log(dat);
-  // document.getElementById("output").innerText = data[branch[trgt]];
+  return dat;
 }
 function init() {
   document.getElementById("output").innerText = "";
@@ -80,8 +139,7 @@ function read_file(input) {
       skipEmptyLines: true,
       // newline: "\n", // Let PapaParse auto-detect newlines
       complete: function (results) {
-        console.log(results.data);
-        read_data(results.data);
+        data_file.push(results.data);
       },
       error: function (error) {
         console.error("Error parsing CSV:", error);
@@ -89,3 +147,4 @@ function read_file(input) {
     });
   }
 }
+create_table();
